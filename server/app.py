30caf80536644d3f49ccbdb35facc20ae2906539
TJ Stifter <tjstifter@gmail.com>
2023-08-03 11:22:55 -0400

    completes lab

--- server/app.py
+++ server/app.py
@@ -48,6 +48,28 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username == request.get_json()['username']).first()
+        session['user_id'] = user.id
+        return make_response(user.to_dict())
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] = None
+        return make_response({}, 204)
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session['user_id']).first()
+        if user:
+            return make_response(user.to_dict())
+        else:
+            return make_response({}, 401)
+
+api.add_resource(CheckSession, '/check_session')
+api.add_resource(Logout, '/logout')
+api.add_resource(Login, '/login')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
